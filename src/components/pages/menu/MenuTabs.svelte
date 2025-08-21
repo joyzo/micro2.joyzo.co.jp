40853a4c6cb42cefdd83e7e01affc06d17dddbbe
Daimon, Junpei <j-daimon@joyzo.co.jp>
2025-08-21 09:28:51 +0900

    refactor: 不要なページ・コンポーネントの削除 - gallery、menu、shopディレクトリを削除 - サイトマップに必要なページのみに整理

--- src/components/pages/menu/MenuTabs.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import type { MenuObject } from "src/utils/buildMenuObject";
-  import Tabs from "./Tabs/Tabs.svelte";
-
-  export let menu: MenuObject;
-</script>
-
-<Tabs {menu} />
